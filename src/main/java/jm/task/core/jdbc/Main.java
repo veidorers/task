a54e4c0858072d63a58952af06048a53d87a5b28
Sergey Fomenko <artem.work749@gmail.com>
2023-12-20 16:35:13 +0300

    Finish JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,22 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args)  {
-        var userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.getAllUsers().forEach(System.out::println);
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.getAllUsers().forEach(System.out::println);
+        var userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Mark", "Zuckerberg", (byte) 39);
+        userService.saveUser("Tim", "Cook", (byte) 63);
+        userService.saveUser("Jeff", "Bezos", (byte) 59);
+        userService.saveUser("Sergey", "Brin", (byte) 50);
+
+        var users = userService.getAllUsers();
+        users.forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
