e43869905bc801082438a4e32cfc27ee824be314
Sergey Fomenko <artem.work749@gmail.com>
2023-12-21 16:38:03 +0300

    Add UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,34 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.HibernateUtil;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
 public class Main {
     public static void main(String[] args)  {
-        var session = HibernateUtil.buildSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        System.out.println(session.get(User.class, 3L));
-        session.getTransaction().commit();
+        var userDaoHibernate = new UserDaoHibernateImpl();
+
+        userDaoHibernate.dropUsersTable();
+
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.createUsersTable();
+
+        userDaoHibernate.saveUser("Mark", "Zuckerberg", (byte) 39);
+        userDaoHibernate.saveUser("Tim", "Cook", (byte) 63);
+        userDaoHibernate.saveUser("Jeff", "Bezos", (byte) 59);
+        userDaoHibernate.saveUser("Sergey", "Brin", (byte) 50);
+
+        userDaoHibernate.removeUserById(3L);
+        userDaoHibernate.removeUserById(3L);
+
+        var users = userDaoHibernate.getAllUsers();
+        users.forEach(System.out::println);
+
+
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
+
+        userDaoHibernate.getAllUsers().forEach(System.out::println);
+
+        userDaoHibernate.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.HibernateUtil;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -12,31 +14,74 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+            session.createNativeQuery("""
+                CREATE TABLE IF NOT EXISTS users (
+                    id BIGINT AUTO_INCREMENT PRIMARY KEY ,
+                    name VARCHAR(128),
+                    last_name VARCHAR(128),
+                    age TINYINT
+                );
+                """).executeUpdate();
 
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            session.persist(new User(name, lastName, age));
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            var user = session.get(User.class, id);
+            if(user != null) {
+                session.remove(user);
+            }
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+
+            return session.createQuery("SELECT u FROM User u").getResultList();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        try (var session = HibernateUtil.buildSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
 
+            session.createNativeQuery("TRUNCATE users").executeUpdate();
+
+            session.getTransaction().commit();
+        }
     }
 }
